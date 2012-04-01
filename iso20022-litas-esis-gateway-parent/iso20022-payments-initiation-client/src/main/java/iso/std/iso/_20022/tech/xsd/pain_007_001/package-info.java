ac470a9984366600d870ded1763ac2c64ee6ff43
Veniamin Goldin <vgoldin@gmail.com>
2012-04-01 11:57:24 +0300

    Remove as part of the refactoring

--- iso20022-litas-esis-gateway-parent/iso20022-payments-initiation-client/src/main/java/iso/std/iso/_20022/tech/xsd/pain_007_001/package-info.java
+++
@@ -1,2 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "urn:iso:std:iso:20022:tech:xsd:pain.007.001.02", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
-package iso.std.iso._20022.tech.xsd.pain_007_001;
--- iso20022-litas-esis-gateway-parent/iso20022-payments-initiation-client/src/main/java/lt/lba/xmlns/unifi/services/foundation/faults/v01/package-info.java
+++
@@ -1,2 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "urn:lt:lba:xmlns:unifi:services:foundation:faults:V01", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
-package lt.lba.xmlns.unifi.services.foundation.faults.v01;
