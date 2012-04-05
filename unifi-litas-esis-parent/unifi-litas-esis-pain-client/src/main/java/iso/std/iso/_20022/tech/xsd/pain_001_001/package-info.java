c51afff7cbd116379b43138d561bb7653e57e2b7
Veniamin Goldin <vgoldin@gmail.com>
2012-04-05 22:25:52 +0300

    Move types closer to builder

--- unifi-litas-esis-parent/unifi-litas-esis-pain-client/src/main/java/iso/std/iso/_20022/tech/xsd/pain_001_001/package-info.java
+++
@@ -1,2 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "urn:iso:std:iso:20022:tech:xsd:pain.001.001.03", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
-package iso.std.iso._20022.tech.xsd.pain_001_001;
--- unifi-litas-esis-parent/unifi-litas-esis-pain-client/src/main/java/iso/std/iso/_20022/tech/xsd/pain_008_001/package-info.java
+++
@@ -1,2 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "urn:iso:std:iso:20022:tech:xsd:pain.008.001.02", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
-package iso.std.iso._20022.tech.xsd.pain_008_001;
