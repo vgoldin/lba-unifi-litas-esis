5ad3c582d70fdadde51af3eb5a90f90843d62a97
Veniamin Goldin <vgoldin@gmail.com>
2012-04-05 22:17:56 +0300

    Move closer to Type Converter
    * Move Columns names constants closer to converter
    * Change naming to reflect LITAS-ESIS

---
+++ unifi-litas-esis-parent/unifi-litas-esis-gateway-app/src/main/java/lt/itdbaltics/unifi/gateway/Columns.java
@@ -0,0 +1,27 @@
+package lt.itdbaltics.unifi.gateway;
+
+public class Columns {
+	public static final String DocumentNumber = "DocumentNumber";
+	public static final String ValueDate = "ValueDate";
+	public static final String Priority = "Priority";
+	public static final String PayerAccount = "PayerAccount";
+	public static final String PayerName = "PayerName";
+	public static final String PayerId = "PayerId";
+	public static final String PayerCreditInstitution = "PayerCreditInstitution";
+	public static final String CustomerCodeInBeneficiaryIs = "CustomerCodeInBeneficiaryIs";
+	public static final String OriginatorAccount = "OriginatorAccount";
+	public static final String OriginatorName = "OriginatorName";
+	public static final String OriginatorId = "OriginatorId";
+	public static final String BeneficiaryAccount = "BeneficiaryAccount";
+	public static final String BenificiaryName = "BenificiaryName";
+	public static final String BenificiaryId = "BenificiaryId";
+	public static final String BenificiaryCreditInsitution = "BenificiaryCreditInsitution";
+	public static final String CustomerCodeInPayerIs = "CustomerCodeInPayerIs";
+	public static final String BeneficiaryPartyAccount = "BeneficiaryPartyAccount";
+	public static final String BenificiaryPartyName = "BenificiaryPartyName";
+	public static final String BenificiaryPartyId = "BenificiaryPartyId";
+	public static final String Amount = "Amount";
+	public static final String CurrencyCode = "CurrencyCode";
+	public static final String ReferenceNo = "ReferenceNo";
+	public static final String PaymentDetails = "PaymentDetails";
+}
